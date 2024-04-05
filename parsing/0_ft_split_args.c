af1c1a9ce998e3a5e45d72e90a3a99db8687479c
aezzahir <aero.glowsky@gmail.com>
2024-04-05 02:55:08 +0000

    aezzahir

--- parsing/0_ft_split_args.c
+++ parsing/0_ft_split_args.c
@@ -31,6 +31,32 @@ char *ft_handle_dquote(char *input, int *start, int *end)
     ft_handle_dquote(input, start, end);
     return(input);
 }
+
+char *ft_handle_quote(char *input, int *start, int *end)
+{
+    char *token;
+
+    *end = *start + 1;
+    while (input && input[*end])
+    {
+        if (input[*end] == '\'')
+        {
+            *end =*end + 1;
+            token = ft_substrdup(input, start, end);
+            printf("%s\n", token);
+            *start = *end;
+            return (token);
+        }
+        else
+           *end =*end + 1;
+    }
+    *end = *start;
+    token = input;
+    input = ft_strjoin(input, readline("quote>"));
+    free(token);
+    ft_handle_dquote(input, start, end);
+    return(input);
+}
 void ft_split_args(char *input)
 {
     char *token;
@@ -51,7 +77,11 @@ void ft_split_args(char *input)
         }
         else if (input[end] == '"' && input[end - 1] == ' ') // check input[end - 1] accessbility and it is a space;
         {
-            token = ft_handle_dquote(input, &start, &end);
+            token = ft_handle_dquote(input, &start, &end);  
+        }
+        else if (input[end] == '\'' && (end == 0 || input[end - 1] == ' ')) // check input[end - 1] accessbility and it is a space;
+        {
+            token = ft_handle_quote(input, &start, &end);  
         }
         else if (!ft_strncmp(&input[end], ">>", 2) || !ft_strncmp(&input[end], "<<", 2))
         {
