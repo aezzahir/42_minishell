0176ceb8b678f4d9fcde6a41aa8e5871841f2795
aezzahir <aero.glowsky@gmail.com>
2024-04-06 13:09:12 +0000

    parssing 1 done Now we should save each new token inside a node of linked list then we pass to the 2nd part of parssing to the cmd1->cmd2->cmd3 ...

--- parsing/0_ft_split_args.c
+++ parsing/0_ft_split_args.c
@@ -5,31 +5,74 @@
  * each pointing to a separate token (command, argument, operator)
  * @cmd_line: Command line
  * @return: Array of tokens
+ * 
+ * 
 */
+char *ft_handle_envar(char *token)
+{
+    int     i = 0;
+    int     start = 0;
+    int     end = 0;
+    char    *var_name;
+    char    *left;
+    char    *right;
+    var_name = NULL;
+
+    while (token && token[i])
+    {
+        if (token[i] == '$')
+        {
+            start = i + 1;
+            end = i + 1;
+            while (ft_isalnum(token[end]))
+            {
+                end++;
+            }
+            i = end - 1;
+            if (1)
+            {
+                var_name = ft_substr(token, start, end - start); //
+                var_name = getenv(var_name);
+                left = ft_strjoin(ft_substr(token, 0, start - 1), var_name);
+                right = ft_strjoin(left, ft_substr(token, end, ft_strlen(token) - end));
+                free(token);
+                free(left);
+                token = right;
+                return (token);
+            }
+        }
+        i++;
+    }
+    return (token);
+}
 char *ft_handle_dquote(char *input, int *start, int *end)
 {
     char *token;
 
-    *end = *start +1;
+    *end = *start + 1;
+    token = NULL;
     while (input && input[*end])
     {
         if (input[*end] == '"')
         {
             *end =*end + 1;
             token = ft_substrdup(input, start, end);
-            printf("%s\n", token);
+        
             *start = *end;
             return (token);
         }
         else
            *end =*end + 1;
     }
-    *end = *start;
-    token = input;
-    input = ft_strjoin(input, readline("dquote>"));
-    free(token);
-    ft_handle_dquote(input, start, end);
-    return(input);
+//    //*end = *start;
+//     token = input;
+//     input = ft_strjoin(input, "\n");
+//     free(token);
+//     token = input;
+//     input = ft_strjoin(input, readline("dquote>"));
+//     free(token);
+//     ft_handle_dquote(input, start, end);
+    return(token);
 }
 
 char *ft_handle_quote(char *input, int *start, int *end)
@@ -37,25 +80,20 @@ char *ft_handle_quote(char *input, int *start, int *end)
     char *token;
 
     *end = *start + 1;
+    token = NULL;
     while (input && input[*end])
     {
         if (input[*end] == '\'')
         {
             *end =*end + 1;
             token = ft_substrdup(input, start, end);
-            printf("%s\n", token);
             *start = *end;
             return (token);
         }
         else
            *end =*end + 1;
     }
-    *end = *start;
-    token = input;
-    input = ft_strjoin(input, readline("quote>"));
-    free(token);
-    ft_handle_dquote(input, start, end);
-    return(input);
+    return(token);
 }
 void ft_split_args(char *input)
 {
@@ -70,37 +108,52 @@ void ft_split_args(char *input)
             if (end > start)
             {
                 token = ft_substrdup(input, &start, &end);
+                token = ft_handle_envar(token);//to be updated 1111
                 printf("%s\n", token);
             }
             end++;
             start = end;
         }
-        else if (input[end] == '"' && input[end - 1] == ' ') // check input[end - 1] accessbility and it is a space;
+        else if (input[end] == '"' && (end == 0 || (end > 0 && input[end - 1] == ' '))) // check input[end - 1] accessbility and it is a space;
         {
-            token = ft_handle_dquote(input, &start, &end);  
+            token = ft_handle_dquote(input, &start, &end);
+            token = ft_handle_envar(token);//to be updated 1111
+            printf("%s\n", token); 
         }
-        else if (input[end] == '\'' && (end == 0 || input[end - 1] == ' ')) // check input[end - 1] accessbility and it is a space;
+        else if (input[end] == '\'' && (end == 0 || (end > 0 && input[end - 1] == ' '))) // check input[end - 1] accessbility and it is a space;
         {
-            token = ft_handle_quote(input, &start, &end);  
+            token = ft_handle_quote(input, &start, &end);
+            token = ft_handle_envar(token);//to be updated  1111
+            printf("%s\n", token);
         }
         else if (!ft_strncmp(&input[end], ">>", 2) || !ft_strncmp(&input[end], "<<", 2))
         {
             end = end + 2;
             token = ft_substrdup(input, &start, &end);
-            printf("%s\n", token);
             start = end;
+            token = ft_handle_envar(token);//to be updated  ---1111
+            printf("%s\n", token);
         }
         else if (input[end] == '|' || input[end] == '>' || input[end] == '<')
         {
             end = end + 1;
             token = ft_substrdup(input, &start, &end);
-            printf("%s\n", token);
             start = end;
+            token = ft_handle_envar(token);//to be updated  ---1111
+            printf("%s\n", token);
         }
+        // else if (input[end] == '$')
+        // {
+        //     end = end + 1;
+        //     token = ft_substrdup(input, &start, &end);
+        //    
+        //     start = end;
+        // }
         else
             end++;
         //printf("start = %d; end = %d\n", start, end);
     }
     token = ft_substrdup(input, &start, &end);
+    token = ft_handle_envar(token);//to be updated  ---1111
     printf("%s\n", token);
 }
