1862c2c69e2a4083173c50342b644df10315dfbc
aezzahir <aero.glowsky@gmail.com>
2024-04-16 22:12:36 +0100

    parsing ...

--- parsing/1_parse_cmds.c
+++ parsing/1_parse_cmds.c
@@ -6,7 +6,7 @@ int ft_is_what(char *token_content)
     if (!token_content)
         return (-1);
     else if (ft_strncmp(token_content, "|", ft_strlen(token_content)) == 0)
-        return (1);
+        return (0);
     else if (ft_strncmp(token_content, "<", ft_strlen(token_content)) == 0)
         return (2);
     else if (ft_strncmp(token_content, ">", ft_strlen(token_content)) == 0)
@@ -16,20 +16,33 @@ int ft_is_what(char *token_content)
     else if (ft_strncmp(token_content, "<<", ft_strlen(token_content)) == 0)
         return (5);
     else
-        return (0);
+        return (1);
+}
+
+void ft_add_arg_to_cmd(t_cmd *cmd, t_list *token)
+{
+    
 }
 
 
+void ft_handle_special_tok(t_cmd *cmd, t_list *token)
+{
+    
+}
 
-
+void ft_add_cmd(t_prompt *prompt, t_cmd   *cmd)
+{
+    ft_lstadd_back(prompt->cmds, ft_lstnew((void *)cmd));
+    
+}
 
 
 t_prompt *ft_parse_cmds(t_list **tokens_list)
 {
     t_prompt *prompt;
     t_list  *token;
-   
     t_cmd   *cmd;
+    char    *cmd_args;
 
     if (!tokens_list)
     {
@@ -39,26 +52,20 @@ t_prompt *ft_parse_cmds(t_list **tokens_list)
     prompt = (t_prompt *)malloc(sizeof(t_prompt));
     if(!prompt)
         return (NULL); // exit failaire exit(1)
-   
     while (token)
     {
-        if (num_of_cmds == 0)
+        cmd = (t_cmd *)malloc(sizeof(t_cmd));
+        if(!cmd)
+            return (NULL);
+        while (ft_is_what(token) >= 1)
         {
-            num_of_args = ft_get_args_num(token);
-            (cmd->content)
-            printf("cmd (%d): num of args = %d\n", num_of_cmds, num_of_args);
-        }
-        else if (ft_what_is_in_token((char *)(token->content)) == 1)
-        {
-            if (!(token->next) || ft_what_is_in_token((char *)(token->next->content)) != 0)
-            {
-                printf("parse error near %s\n", (char *)(token->content));
-            }
+            if (ft_is_what(token) == 1)
+                ft_add_arg_to_cmd(cmd, token);
+            else
+                ft_handle_special_tok(cmd, token);
             token = token->next;
-            num_of_args = ft_get_args_num(token);
-            num_of_cmds++;
-            printf("cmd (%d): num of args = %d\n", num_of_cmds, num_of_args);
         }
+        ft_add_cmd(prompt, cmd);
         token = token->next;
     }
 }
