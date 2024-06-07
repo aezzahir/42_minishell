1e0eb9f1218c1da5e8275dd61c1c11d646ea6cc5
aezzahir <aero.glowsky@gmail.com>
2024-06-07 22:58:09 +0100

    parsing

--- parsing/handle_specials/ft_handle_quotes.c
+++ parsing/handle_specials/ft_handle_quotes.c
@@ -1,55 +1,68 @@
 #include "../../minishell.h"
 extern int	g_status;
+
 /**
  * ft_handle_quote: Handles quoted strings
+ * @tokens_list: List of tokens
+ * @envp: Environment variables
  * @input: Input string
  * @start: Pointer to start index (updated to point after the opening quote)
  * @end: Pointer to end index (updated to point to the closing quote)
  * @quote: Quote character ('\"' or '\'')
- * @return: Dynamically allocated quoted substring (NULL if error occurred)
+ * @return: TRUE if successful, FALSE otherwise
  */
-
 int ft_handle_quote(t_list **tokens_list, char **envp, char *input, int *start, int *end, char quote)
 {
-    char *token;
     char    *left;
     char    *right;
-    
-    // printf("in handle quotes %d\n", *start);
-    token = NULL;
-    left = NULL;
+    int     in_quote;
 
-    if (!(*end > 1 && ft_iswhitespace(input[*end - 1])))
-        left = ft_substrdup(input, start, end);
-    *start = *end + 1;
-    if (ft_is_special_token(ft_lstlast(*tokens_list)) == HERDOC)
+    left = NULL;
+    right = NULL;
+    in_quote = TRUE;
+
+    left = ft_substrdup(input, start, end);
+    *end = *end + 1;
+    *start = *end;
+
+    while (input[*end])
     {
-        right = ft_strjoin("'", left);
-        free(left);
-        left = right;
-        right = NULL;
-    }
-    while (input && input[*end])
-    {
-        *end =*end + 1;
-        if (input[*end] == quote)
+        if (in_quote && input[*end] == quote)
         {
             right = ft_substrdup(input, start, end);
-            token = ft_strjoin(left, right);
-            free(left);
+            left = ft_strjoin(left, right);
             free(right);
+            in_quote = FALSE;
+            *start = *end + 1;
+
             if (quote == '"')
             {
-                token = ft_handle_envar(token, envp);
-                token = ft_exit_status(token);  
+                left = ft_handle_envar(left, envp);
+                left = ft_exit_status(left);
             }
-            ft_add_token(tokens_list, token);
-            *end =*end + 1;
+        }
+        else if (!in_quote && (input[*end] == '"' || input[*end] == '\''))
+        {
+            quote = input[*end];
+            in_quote = TRUE;
+            *start = *end + 1;
+        }
+        else if (!input[*end + 1] || (!in_quote && ft_iswhitespace(input[*end + 1])))
+        {
+            *end = *end + 1;
+            right = ft_substrdup(input, start, end);
+            left = ft_strjoin(left, right);
+            free(right);
             *start = *end;
+            ft_add_token(tokens_list, left);
             return (TRUE);
-        }   
+        }
+
+        *end = *end + 1;
     }
+
     if (left)
-        free(left);
-    return (FALSE);
+        ft_add_token(tokens_list, left);
+
+    return (TRUE);
 }
